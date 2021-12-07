0bb1cc6a8ddde5dad70035f040a690ad28f3ded3
NickNocc <haretakis4@gmail.com>
2021-12-07 14:25:46 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 // const generatePage = require('./src/page-template');
 const inquirer = require(`inquirer`);
 
-inquirer
-  .prompt([
-    {
-      type: `input`,
-      name: `name`,
-      message: `What is your name?`
-    }
-  ])
-  .then(answers => console.log(answers));
+  const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: `input`,
+        name: `name`,
+        message: `What is your name?`  
+      }
+    ]);
+  };
+
+  promptUser().then (answers => console.log(answers));
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile('./index.html', pageHTML, err => {
