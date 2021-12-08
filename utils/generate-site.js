7a6be1738dc7fa67a3dc3e845f675d8f31c246e6
NickNocc <haretakis4@gmail.com>
2021-12-08 12:44:48 -0800

    Update generate-site.js

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -16,18 +16,19 @@ const writeFile = fileContent => {
 };
 
 const copyFile = () => {
-    return new Promise((resolve, reject) =>{
-        fs.copyFile("./dist/index.html", err => {
-            if (err) {
-                reject (err);
-                return
-            }
-            resolve({
-                ok: true,
-                message: "File copied!"
-            });
+    return new Promise((resolve, reject) => {
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve({
+          ok: true,
+          message: 'Stylesheet created!'
         });
+      });
     });
-};
+  };
 
 module.exports = { writeFile, copyFile };
